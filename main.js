cb3d8b278bb3d2ae8d9d295ae3e8b374cbf727a1
NiakaGaming <niaka-ishigame@hotmail.com>
2025-01-06 19:18:20 +0100

    Load inventory visuals at start && Items description on mouse over
    Move item's description to the left if goes outside of viewport.

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ player = {
         secondHand: "wooden shield",
     },
     inventory: [
-        "",
-        "",
-        "",
-        "",
+        "Minor health potion",
+        "Minor health potion",
+        "Minor health potion",
+        "Major health potion",
         "",
         "",
         "",
@@ -76,6 +76,18 @@ window.onload = function () {
     characterLevel.textContent = player.stats.level;
     characterExp.textContent = player.stats.experience + " / " + levels[player.stats.level];
     characterHpBar.textContent = player.stats.hp + " / " + player.stats.maxHp;
+
+    // Create inventory visuals for existing inventory
+    player.inventory.forEach((element, index) => {
+        if (element != "") {
+            items.forEach(elem => {
+                if (elem.label == element) {
+                    let newItem = document.getElementsByClassName("inv-" + index)[0];
+                    createItem(newItem, elem.label, elem.src, elem.description);
+                }
+            });
+        }
+    });
 };
 
 // BUTTONS
@@ -107,7 +119,6 @@ buttonQuit.addEventListener("click", (e) => {
     startPanel.style = "display:block";
 });
 
-
 buttonHunt.addEventListener("click", (e) => {
     if (huntResult.style.visibility != "visible" || huntResult.style.visibilityvisibility == "") {
         huntResult.style = "visibility:visible"
@@ -119,19 +130,21 @@ buttonHunt.addEventListener("click", (e) => {
     }
 
 });
+
+// Close Hunt result Panel
 buttonCloseHunt.addEventListener("click", (e) => {
     if (huntResult.style.visibility == "visible") {
         huntResult.style = "visibility:hidden"
     }
 });
 
+// Inventory item on Click
 inventory.addEventListener("click", (e) => {
     if (e.target.tagName == "IMG") {
-
         // Get parent class (inv-x) to find the inventory index
-        let itemParent = e.target.parentElement.classList[1];
-        let inventoryIndex = itemParent.charAt(itemParent.length - 1);
-        let itemToUse;
+        let itemParent = e.target.parentElement.classList[1],
+            inventoryIndex = itemParent.charAt(itemParent.length - 1),
+            itemToUse;
 
         // Find the item in the Items array and store it in itemToUse
         items.forEach(element => {
@@ -153,12 +166,35 @@ inventory.addEventListener("click", (e) => {
 
             // Remove item in DB
             player.inventory[inventoryIndex] = "";
-            // Remove item visual
+            // Remove item visual (img & description)
+            e.target.nextSibling.remove();
             e.target.remove();
         }
         else if (itemToUse.type == "buff") {
 
         }
+    }
+});
+
+// Item Description move on mouse hover
+inventory.addEventListener("mousemove", (e) => {
+    let desc = e.target.nextSibling,
+        descChilds = desc.childNodes;
+    if (e.target.tagName == "IMG") {
+        var x = e.clientX,
+            y = e.clientY;
+        desc.style.left = (x + 20) + "px";
+        desc.style.top = (y + 20) + "px";
+
+        // Check if Span is outside of viewport
+        let rect = desc.getBoundingClientRect();
+        // Span right side compared to viewport width
+        if ((rect.right) > window.innerWidth) {
+            desc.style.left = (x - desc.clientWidth - 10) + "px";
+        }
+        // More conditions (left side, top side, bottom side)
+        // (rect.x + rect.width) < 0 || (rect.y + rect.height) < 0
+        // (rect.x > window.innerWidth || rect.y > window.innerHeight)
     }
 });
 
@@ -185,43 +221,57 @@ function fight() {
     characterHpBar.textContent = player.stats.hp + " / " + player.stats.maxHp;
     hpMax.style.width = (player.stats.hp / player.stats.maxHp) * 100 + "%";
 
-    // If maxHp <= 0 THEN DEAD
+    // ----------------------- //
+    // If maxHp <= 0 THEN DEAD //
+    // ----------------------- //
 }
 
 // Check item's luck to see if it's droped
 function getItemsLuck() {
     items.forEach(element => {
         if (element.chance >= Math.random() * 100) {
-            itemDrop(element.label, element.src);
+            itemDrop(element.label, element.src, element.description);
         }
     });
 }
 
-function itemDrop(itemLabel, itemSrc) {
+function itemDrop(itemLabel, itemSrc, itemDesc) {
     // Add item in DB
     // Check available space in inventory and add item if empty
-    let newIndex = -1;
-    let isEmpty = true;
+    let newIndex = -1,
+        isEmpty = true;
     player.inventory.forEach((element, index) => {
         if (element == "" && isEmpty == true) {
             newIndex = index;
             isEmpty = false;
         }
     });
+
     // If newIndex != -1 (if not full) => add items in the right place in DB
     // And add it in the right place in inventory
     let newItem = document.getElementsByClassName("inv-" + newIndex)[0];
-
     if (newIndex != -1 && !newItem.hasChildNodes()) {
+        // Add item in DB
         player.inventory[newIndex] = itemLabel;
         // Create HTML Visual
         // Item index == Inventory place
-        newItem.appendChild(document.createElement("img"));
-
-        newItem.childNodes[0].classList.add("inv-item");
-        newItem.childNodes[0].src = itemSrc;
+        createItem(newItem, itemLabel, itemSrc, itemDesc);
     }
     else {
         console.log("Inventory Full");
     }
+}
+
+function createItem(newItem, itemLabel, itemSrc, itemDesc) {
+    // Create Item IMG
+    newItem.appendChild(document.createElement("img"));
+    newItem.childNodes[0].classList.add("inv-item");
+    newItem.childNodes[0].src = itemSrc;
+    // Create Item Decsription (H1 label & P description)
+    newItem.appendChild(document.createElement("span"));
+    newItem.childNodes[1].classList.add("item-description");
+    newItem.childNodes[1].appendChild(document.createElement("h1"));
+    newItem.childNodes[1].childNodes[0].textContent = itemLabel;
+    newItem.childNodes[1].appendChild(document.createElement("p"));
+    newItem.childNodes[1].childNodes[1].textContent = itemDesc;
 }
